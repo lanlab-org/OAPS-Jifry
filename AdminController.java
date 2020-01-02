10ad725ff8a990161efab19706e0c2900d7fa739
MJH Mohamed <48899642+mjhmohamed@users.noreply.github.com>
2020-01-02 17:50:03 +0800

    Updated servlet

--- AdminController.java
+++ AdminController.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.swing.JOptionPane;
 
 import com.database.DB;
 import com.javaBean.Article;
@@ -158,21 +159,15 @@ public class AdminController extends HttpServlet {
 			String title = request.getParameter("title");
 			String highlights = request.getParameter("highlights");
 			String abstracts = request.getParameter("abstracts");
-			Timestamp time = new Timestamp(System.currentTimeMillis()); 
+			Timestamp time = new Timestamp(System.currentTimeMillis());
 			
-			
-			Article a = new Article();
-			a.setTitle(title);
-			a.setHighlights(highlights);
-			a.setAbstracts(abstracts);
-			a.setTime(time);
-			a.setOldtitle(oldtitle);
-			
+
 			DB db = new DB();
+/*			boolean checkstatus = false;
 			
 			try
 			{
-				db.updateArticle(a);
+				checkstatus = db.checktitle(title);	
 			}
 			
 			catch(SQLException e)
@@ -180,7 +175,70 @@ public class AdminController extends HttpServlet {
 				e.printStackTrace();
 			}
 			
-			request.getRequestDispatcher("AdminHome.jsp").forward(request, response);
+			if(checkstatus)
+			{
+				JOptionPane.showMessageDialog(null, "Article Title is already exist please try anotherone ", "Info", JOptionPane.INFORMATION_MESSAGE);
+				
+				request.setAttribute("title", title);
+				request.setAttribute("highlights", highlights);
+				request.setAttribute("abstracts", abstracts);
+				request.getRequestDispatcher("AdminEdit.jsp").forward(request, response);
+			}
+			
+			else
+			{				
+*/			
+			
+			if(title.length() > 100)
+			{
+				JOptionPane.showMessageDialog(null, "you have typed more than 100 letters in Highlights", "Info", JOptionPane.INFORMATION_MESSAGE);
+				
+				request.setAttribute("title", oldtitle);
+				request.getRequestDispatcher("AdminEdit.jsp").forward(request, response);
+			}
+			
+			else if(highlights.length() > 250)
+			{
+				JOptionPane.showMessageDialog(null, "you have typed more than 250 letters in Highlights", "Info", JOptionPane.INFORMATION_MESSAGE);
+				
+				request.setAttribute("title", oldtitle);
+				request.getRequestDispatcher("AdminEdit.jsp").forward(request, response);
+			}
+			
+			else if(abstracts.length() > 999)
+			{
+				JOptionPane.showMessageDialog(null, "you have typed more than 1000 letters in abstract", "Info", JOptionPane.INFORMATION_MESSAGE);
+				
+				request.setAttribute("title", oldtitle);
+				request.getRequestDispatcher("AdminEdit.jsp").forward(request, response);
+			}
+			
+			else
+			{
+				
+				Article a = new Article();
+				a.setTitle(title);
+				a.setHighlights(highlights);
+				a.setAbstracts(abstracts);
+				a.setTime(time);
+				a.setOldtitle(oldtitle);
+				
+//				DB db = new DB();
+				
+				try
+				{
+					db.updateArticle(a);
+				}
+				
+				catch(SQLException e)
+				{
+					e.printStackTrace();
+				}
+				
+				request.getRequestDispatcher("AdminHome.jsp").forward(request, response);
+			}
+//		} else
+			
 		}
 		
 		if(page.equals("delete"))
@@ -200,6 +258,114 @@ public class AdminController extends HttpServlet {
 			}
 			
 			request.getRequestDispatcher("AdminHome.jsp").forward(request, response);
+		}
+		
+
+//----------------------------------------------------------------------------------------------
+// master function		
+		
+		if(page.equals("master-home"))
+		{
+			request.getRequestDispatcher("Master.jsp").forward(request, response);
+		}
+		
+		if(page.equals("master-articles"))
+		{
+			request.getRequestDispatcher("MasterArticles.jsp").forward(request, response);
+		}
+		
+		if(page.equals("master-comments"))
+		{
+			request.getRequestDispatcher("Comments.jsp").forward(request, response);
+		}
+		
+		if(page.equals("master-authors"))
+		{
+			request.getRequestDispatcher("MasterAuthors.jsp").forward(request, response);
+		}
+		
+		if(page.equals("master-delete"))
+		{
+			String title = request.getParameter("title");
+			
+			DB db = new DB();
+			
+			try
+			{
+				db.deleteArticle(title);
+			}
+			
+			catch(SQLException e)
+			{
+				e.printStackTrace();
+			}
+			
+			request.getRequestDispatcher("MasterArticles.jsp").forward(request, response);
+		}
+		
+		if(page.equals("master-view-article"))
+		{
+			String title = request.getParameter("title");
+			
+			request.setAttribute("title", title);
+			request.getRequestDispatcher("MasterViewArticle.jsp").forward(request, response);
+		}
+		
+		if(page.equals("master-delete-comments"))
+		{
+			String comment = request.getParameter("comment");
+			
+			DB db = new DB();
+			
+			try
+			{
+				db.deleteComment(comment);
+			}
+			
+			catch(SQLException e)
+			{
+				e.printStackTrace();
+			}
+			
+			request.getRequestDispatcher("Comments.jsp").forward(request, response);
+		}
+		
+		if(page.equals("master-block-author"))
+		{
+			String author = request.getParameter("author");
+			
+			DB db = new DB();
+			
+			try
+			{
+				db.blockAuthor(author);
+			}
+			
+			catch(SQLException e)
+			{
+				e.printStackTrace();
+			}
+			
+			request.getRequestDispatcher("blocklist.jsp").forward(request, response);
+		}
+		
+		if(page.equals("master-release-author"))
+		{
+			String author = request.getParameter("author");
+			
+			DB db = new DB();
+			
+			try
+			{
+				db.releaseAuthor(author);
+			}
+			
+			catch(SQLException e)
+			{
+				e.printStackTrace();
+			}
+			
+			request.getRequestDispatcher("MasterAuthors.jsp").forward(request, response);
 		}
 		
 	}
